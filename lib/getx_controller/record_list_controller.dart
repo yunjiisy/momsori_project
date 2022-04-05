13f7f5f48e301010c53f150daed1ac51d31b8bd8
_smcow <phw2088@gmail.com>
2022-04-05 18:11:40 +0900

    storage screen update

--- lib/getx_controller/record_list_controller.dart
+++ lib/getx_controller/record_list_controller.dart
@@ -1,17 +1,21 @@
+import 'dart:io';
+
+import 'package:path_provider/path_provider.dart';
 import 'package:get/get.dart';
 
 class RecordListController extends GetxController {
+  callDirectoryList() async {
+    var tempDir = await getExternalStorageDirectory();
+    var directoryEx =
+        Directory('${tempDir!.parent.parent.parent.parent.path}/momsound/');
+    List<FileSystemEntity> entries = await directoryEx.list().toList();
+    return entries;
+  }
+
+  List<Map> categoryData = [
+    {'name' : '모든 녹음', 'path' : '/storage/emulate/0/momsound/', 'checked' : false},
+  ];
   List<String> categories = [
-    '카테고리1',
-    '카테고리2',
-    '카테고리3',
-    '카테고리4',
-    '카테고리5',
-    '카테고리6',
-    '카테고리7',
-    '카테고리8',
-    '카테고리9',
-    '카테고리10',
     '+ 카테고리 추가',
   ];
 
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
